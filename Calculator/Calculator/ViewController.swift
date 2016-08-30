f64fe0e3cee2dc0da0dc004fa0cd9a18c7a296e1
bryan0515 <bc2197@nyu.edu>
2016-08-30 18:34:56 -0400

    build up first assignment
    First assignment, calculator demonstrated in lecture 1 and 2.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Bryan Cheng on 2016/8/30.
+//  Copyright (c) 2016年 Bryan Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
